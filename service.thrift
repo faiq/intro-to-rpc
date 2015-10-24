fab38436a3e85cccd241b7f64af482418a9792e4
faiq <faiqrazarizvi@gmail.com>
2015-10-24 19:08:55 -0400

    starts thrift file

---
+++ service.thrift
@@ -0,0 +1,7 @@
+//here we are creating our own type, for readability, "image" which is an array of bytes
+typedef binary image
+
+service MakeTags {
+    // here we are defining a service that has a method generate which returns a list of strings and    // takes in an image
+    list<string> generate(1: image) 
+}
